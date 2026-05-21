cd7c52f8ced1f32a618f8128ca5384895a4807e5
Franco Rossi <Francorossi.visual@gmail.com>
2026-05-21 20:09:24 -0300

    first

---
+++ Client/Obligatorio-N3D-342742-360021-Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Obligatorio_N3D_342742_360021_Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
